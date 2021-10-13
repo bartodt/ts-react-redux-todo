897488d8de84de015351d3680105b2b93dcefb8e
bartodt <bartolome@deltorto.com>
2021-10-13 16:46:31 -0300

    added delete todo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Todo, fetchTodos } from '../actions'
+import { Todo, fetchTodos, deleteTodo } from '../actions'
 import { StoreState } from '../reducers'
 
 interface Props {
   todos: Todo[];
-  fetchTodos(): any
+  fetchTodos: Function;
+  deleteTodo: typeof deleteTodo;
 }
-
 class _App extends Component<Props> {
 
 
@@ -15,9 +15,11 @@ class _App extends Component<Props> {
     this.props.fetchTodos()
   }
 
+  onTodoClick = (id: number): void => { this.props.deleteTodo(id); }
+
   renderList(): JSX.Element[] {
     return this.props.todos.map((todo: Todo) => {
-      return <div key={todo.id}>
+      return <div key={todo.id} onClick={() => this.onTodoClick(todo.id)}>
         {todo.title}
       </div>
     })
@@ -38,4 +40,4 @@ const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
   }
 }
 
-export const App = connect(mapStateToProps, { fetchTodos })(_App)
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App)
